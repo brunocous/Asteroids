d2802a8100d00a0adff87354af2159341d817c1d
Bruno Coussement <coussemetn.bruno@gmail.com>
2013-02-26 12:29:59 +0100

    test

---
+++ src/asteroids/test/ShipTest.java
@@ -0,0 +1,19 @@
+package asteroids.test;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import be.kuleuven.cs.som.annotate.*;
+import asteroids.model.*;
+
+public class ShipTest {
+
+	private Ship 
+	@Before
+	public void setUp() throws Exception{
+		
+	}
+	
+	
+}
