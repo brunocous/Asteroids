313d8bfe7017a0a6553d779f0e8c4bae3afc150a
r0295167 <r0295167@I-PCKS-D-960.luna.kuleuven.be>
2013-03-04 18:07:08 +0100

    facade af

--- src/asteroids/model/Facade.java
+++ src/asteroids/model/Facade.java
@@ -2,103 +2,121 @@ package asteroids.model;
 
 import asteroids.IFacade;
 import asteroids.IShip;
+import asteroids.model.Util.*;
+import asteroids.model.Ship;
+import asteroids.Error.*;
 
 public class Facade implements IFacade{
-	private Ship theShip;
+	
 	
 	public Facade(){
-		theShip = new Ship();
+
 	}
 
 	@Override
 	public IShip createShip() {
 		
-		return null;
+		return new Ship();
 	}
 
 	@Override
 	public IShip createShip(double x, double y, double xVelocity,
 			double yVelocity, double radius, double angle) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return new Ship(new Position(x,y), new Velocity(xVelocity,yVelocity), angle, radius);
 	}
 
 	@Override
 	public double getX(IShip ship) {
-		// TODO Auto-generated method stub
-		return 0;
+		Ship castedShip = (Ship) ship;
+		return castedShip.getPos().getPosX();
 	}
 
 	@Override
 	public double getY(IShip ship) {
-		// TODO Auto-generated method stub
-		return 0;
+		Ship castedShip = (Ship) ship;
+		return castedShip.getPos().getPosY();
 	}
 
 	@Override
 	public double getXVelocity(IShip ship) {
-		// TODO Auto-generated method stub
-		return 0;
+		Ship castedShip = (Ship) ship;
+		return castedShip.getVel().getVelX();
 	}
 
 	@Override
 	public double getYVelocity(IShip ship) {
-		// TODO Auto-generated method stub
-		return 0;
+		Ship castedShip = (Ship) ship;
+		return castedShip.getVel().getVelY();
 	}
 
 	@Override
 	public double getRadius(IShip ship) {
-		// TODO Auto-generated method stub
-		return 0;
+		Ship castedShip = (Ship) ship;
+		return castedShip.getRadius();
 	}
 
 	@Override
 	public double getDirection(IShip ship) {
-		// TODO Auto-generated method stub
-		return 0;
+		Ship castedShip = (Ship) ship;
+		return castedShip.getDirection();
 	}
 
 	@Override
 	public void move(IShip ship, double dt) {
-		// TODO Auto-generated method stub
-		
+		try{ Ship castedShip = (Ship) ship;
+		castedShip.move(dt);
+		} catch(NegativeTimeException ex){
+			throw new ModelException(ex);
+		}
 	}
 
 	@Override
 	public void thrust(IShip ship, double amount) {
-		// TODO Auto-generated method stub
+		try{ Ship castedShip = (Ship) ship;
+		castedShip.thrust(amount);
+		} catch(ExceedsSpeedOfLightException ex){
+			throw new ModelException(ex);
+		}
 		
 	}
 
 	@Override
 	public void turn(IShip ship, double angle) {
-		// TODO Auto-generated method stub
+		Ship castedShip = (Ship) ship;
+		castedShip.turn(angle);
 		
 	}
 
 	@Override
 	public double getDistanceBetween(IShip ship1, IShip ship2) {
-		// TODO Auto-generated method stub
-		return 0;
+		Ship castedShip1 = (Ship) ship1;
+		Ship castedShip2 = (Ship) ship2;
+		return Ship.getDistanceBetween(castedShip1, castedShip2);
 	}
 
 	@Override
 	public boolean overlap(IShip ship1, IShip ship2) {
-		// TODO Auto-generated method stub
-		return false;
+		Ship castedShip1 = (Ship) ship1;
+		Ship castedShip2 = (Ship) ship2;
+		return Ship.overlap(castedShip1, castedShip2);
 	}
 
 	@Override
 	public double getTimeToCollision(IShip ship1, IShip ship2) {
-		// TODO Auto-generated method stub
-		return 0;
+		Ship castedShip1 = (Ship) ship1;
+		Ship castedShip2 = (Ship) ship2;
+		return Ship.getTimeToCollision(castedShip1, castedShip2);
 	}
 
 	@Override
 	public double[] getCollisionPosition(IShip ship1, IShip ship2) {
-		// TODO Auto-generated method stub
-		return null;
+		Ship castedShip1 = (Ship) ship1;
+		Ship castedShip2 = (Ship) ship2;
+		Position colPos = Ship.getCollisionPosition(castedShip1, castedShip2);
+		double[] colPosArray = new double[]{colPos.getPosX() , colPos.getPosY()};
+		
+		return colPosArray;
 	}
 
 }
